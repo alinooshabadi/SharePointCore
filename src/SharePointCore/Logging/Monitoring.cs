8de1d521f03ca297a3bf8daa64a97a61be4f73e1
Ali Nooshabadi <alinooshabadi@users.noreply.github.com>
2018-11-12 16:04:11 +0330

    Change monotoring scope default name

--- src/SharePointCore/Logging/Monitoring.cs
+++ src/SharePointCore/Logging/Monitoring.cs
@@ -7,17 +7,17 @@ namespace SharePointCore.Logging
     {
         public static IDisposable StartHighScope(string name)
         {
-            return new SPMonitoredScope(name, Microsoft.SharePoint.Administration.TraceSeverity.High);
+            return new SPMonitoredScope("EIT Monitoring - " + name, Microsoft.SharePoint.Administration.TraceSeverity.High, new SPSqlQueryCounter());
         }
 
         public static IDisposable StartScope(string name, params ISPScopedPerformanceMonitor[] monitors)
         {
-            return new SPMonitoredScope(name, Microsoft.SharePoint.Administration.TraceSeverity.Verbose, monitors);
+            return new SPMonitoredScope("EIT Monitoring - " + name, Microsoft.SharePoint.Administration.TraceSeverity.Verbose, monitors);
         }
 
         public static IDisposable StartScopeWithTimeLimit(string name, uint milliseconds, params ISPScopedPerformanceMonitor[] monitors)
         {
-            return new SPMonitoredScope(name, milliseconds, monitors);
+            return new SPMonitoredScope("EIT Monitoring - " + name, milliseconds, monitors);
         }
     }
 }
