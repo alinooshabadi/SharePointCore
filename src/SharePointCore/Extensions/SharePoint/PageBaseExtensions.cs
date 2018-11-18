d8f5b4afb32e6ef5f20402637787c1e03119394f
Ali Nooshabadi <ali.nooshabadi@gmail.com>
2018-11-18 21:31:23 +0330

    add PageBaseExtensions class

--- src/SharePointCore/Extensions/SharePoint/PageBaseExtensions.cs
+++ src/SharePointCore/Extensions/SharePoint/PageBaseExtensions.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
-namespace SharePointCore.Extensions.SharePoint
+namespace SharePointCore.Extensions
 {
-    class PageBaseExtensions
+    public static class PageBaseExtensions
     {
+        public static void ChangePageTitle(this Page page, string title)
+        {
+            // Get a reference to the appropriate Content Placeholder
+            var contentPlaceHolder = (ContentPlaceHolder)
+                                   page.Master.FindControl("PlaceHolderPageTitle");
+
+            // Clear out anything that SharePoint might have put in it already
+            contentPlaceHolder.Controls.Clear();
+
+            // Put your content in
+            var literalControl = new LiteralControl();
+            literalControl.Text = title;
+            contentPlaceHolder.Controls.Add(literalControl);
+        }
     }
 }
