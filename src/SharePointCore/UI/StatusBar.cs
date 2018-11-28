fd0a1773e0092f01dad7b1f0575457ad335fac16
Ali Nooshabadi <alinooshabadi@users.noreply.github.com>
2018-11-28 11:30:29 +0330

    fix duplicate notification

--- src/SharePointCore/UI/StatusBar.cs
+++ src/SharePointCore/UI/StatusBar.cs
@@ -10,14 +10,17 @@ namespace SharePointCore.UI
         /// Gets or sets the title of the message (bold).
         /// </summary>
         public string Title { get; set; }
+
         /// <summary>
         /// Gets or sets the text of the message.
         /// </summary>
         public string Text { get; set; }
+
         /// <summary>
         /// Gets or sets, whether the statusbar should render invisible.
         /// </summary>
         public bool HideAtBeginning { get; set; }
+
         /// <summary>
         /// Gets or sets the color of the statusbar.
         /// </summary>
@@ -43,18 +46,22 @@ namespace SharePointCore.UI
             /// No color is set.
             /// </summary>
             None,
+
             /// <summary>
             /// Red background color
             /// </summary>
             Red,
+
             /// <summary>
             /// Blue background color
             /// </summary>
             Blue,
+
             /// <summary>
             /// Green background color
             /// </summary>
             Green,
+
             /// <summary>
             /// Yellow bachground color
             /// </summary>
@@ -73,16 +80,16 @@ namespace SharePointCore.UI
         {
             ScriptLink.RegisterScriptAfterUI(page, "SP.js", false, true);
             var script = string.Format(@"
-               window.onload = function () {{                
+               window.onload = function () {{
                 ExecuteOrDelayUntilScriptLoaded(addStatusBar, 'SP.js');
                 function addStatusBar(){{
                     var sid = SP.UI.Status.addStatus(""{0}"", ""{1}"", {2});
                     {3}
                 }}
-                addStatusBar();
+                //addStatusBar();
                 }}
             ", Title, Text, (!HideAtBeginning).ToString().ToLower(),
-            Color != StatusBarColor.None ? string.Format("SP.UI.Status.setStatusPriColor(sid, '{0}');", Enum.GetName(typeof(StatusBarColor), Color).ToLower()) : "");            
+            Color != StatusBarColor.None ? string.Format("SP.UI.Status.setStatusPriColor(sid, '{0}');", Enum.GetName(typeof(StatusBarColor), Color).ToLower()) : "");
             ScriptManager.RegisterClientScriptBlock(page, typeof(StatusBar), string.Format("StatusBar:{0}", Title), script, true);
         }
     }
